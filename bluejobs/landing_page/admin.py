020e3e4a169346e64405918fd65113aad6fece50
Eldon Dagdag <eldon.dagdag@obf.ateneo.edu>
2024-03-22 01:26:01 +0800

    Registered Student/Department models in admin.py

--- bluejobs/landing_page/admin.py
+++ bluejobs/landing_page/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from landing_page.models import User
+from landing_page.models import User, Student, Department
 '''
 The following code initializes the admin panel for the user credentials in the system. This will help
 keep track of the existing instances of the entities and modify them when necessary.
@@ -8,3 +8,5 @@ Code written by: Nics and Eldon
 '''
 ''
 admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Department)
