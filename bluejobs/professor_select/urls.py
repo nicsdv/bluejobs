a4bc2c24c0e7b898e2adae5f31675998be3ccdf2
Nics De Vega <denise.devega@student.ateneo.edu>
2024-03-25 13:23:23 +0800

    added remove function for course and professor favorite, improved views and functions

--- bluejobs/professor_select/urls.py
+++ bluejobs/professor_select/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from .views import (
     course_select_view,
+    remove_course,
     professor_list_view,
     professor_detail_view,
-    add_professor
+    add_professor,
+    remove_professor
 )
 
 urlpatterns = [
     path('course_select', course_select_view, name = 'course_select'),
+    path('course/<int:pk>/remove', remove_course, name = 'course_remove'),
     path('courses/<int:pk>/professors', professor_list_view, name = 'professor_list'),
     path('courses/<int:course>/professor/<int:pk>/details', professor_detail_view, name = 'professor_details'),
-    path('courses/<int:course>/professor/<int:pk>/add', add_professor, name = 'add_professor')
+    path('courses/<int:course>/professor/<int:pk>/add', add_professor, name = 'add_professor'),
+    path('courses/<int:course>/professor/<int:pk>/remove', remove_professor, name = 'remove_professor'),
 ]
 
 app_name = "professor_select"
