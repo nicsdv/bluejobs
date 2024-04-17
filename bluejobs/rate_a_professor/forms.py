d410c5d534aa6faf23c7964f3356b8f1c275bac6
CHRISTIANNEIL EMMANUEL OCAMPO <christianneil.ocampo@student.ateneo.edu>
2024-04-17 16:52:44 +0800

    Fixed styling bugs across pages

--- bluejobs/rate_a_professor/forms.py
+++ bluejobs/rate_a_professor/forms.py
@@ -7,6 +7,14 @@ class ProfessorRatingForm(forms.ModelForm):
         model = ProfessorRating
         exclude = ['professor', 'student', 'rating_date_time']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['subject_matter_expertise'].widget.attrs['class'] = 'form-score'
+        self.fields['workload_management'].widget.attrs['class'] = 'form-score'
+        self.fields['grading_leniency'].widget.attrs['class'] = 'form-score'
+        self.fields['approachability'].widget.attrs['class'] = 'form-score'
+        self.fields['friendliness'].widget.attrs['class'] = 'form-score'
+
 class ProfessorUpvoteForm(forms.ModelForm):
     class Meta:
         model = Upvotes
